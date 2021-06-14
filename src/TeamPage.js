09bcb631cffb91f0b25062d6c5059a787ff7f3ba
Kenn Hadnot <k_hadnot@yahoo.com>
2021-06-13 17:58:11 -0700

    link to team page works

--- src/TeamPage.js
+++ src/TeamPage.js
@@ -1,63 +1,26 @@
-import React, { Component, useState } from 'react';
-import axios from 'axios';
-import NBA from 'nba';
-import './News.css';
+import React, { Component } from 'react';
+import { useParams } from "react-router-dom";
+//import axios from 'axios';
+//import NBA from 'nba';
 
-class TeamPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            news: []
-        };
-    }
+/** Team news page
+ * 
+ * Renders news and stats for specific team
+ * 
+ * Routed at /teams/:team
+ * 
+ */ 
 
-    async componentDidMount() {
-        let team = 'Portland Trail Blazers';
-        let teamUrl = team.replace(' ', '%').toLowerCase();
-        const API_KEY = 'a6cabd1a61ed4f74b9db44143a8370f5'
-        const url = `https://newsapi.org/v2/everything?q=${teamUrl}&pageSize=5&apiKey=${API_KEY}`;
-        let res = null;
-        try {
-            res = await axios(url, {
-                headers: {
-                    Accept: 'application/json'
-                }
-            });
-        } catch (err) {
-          console.log(err);
-        }
-        this.setState({ 
-            news: res.data.articles
-        });
-    }
-    
-    render() {
-        const dame = NBA.findPlayer('Damian Lillard');
-        const { news } = this.state;
-        console.log(dame); // works, logs info on Dame!!
-        let mappedArr = news.map(news => {
-            return (
-                <div className='container' key={news.title}>
-                    <img src={news.urlToImage} alt={news.title} className='image' />
-                    <a href={news.url} target='_blank' rel='noreferrer'>
-                        <div className='overlay'>
-                            <div className='text'>{news.title}</div>
-                        </div>
-                    </a>
-                </div>
-            );
-        });
-        return (
-            <div className="App">
-                <h3>Welcome to the News Page!!</h3>
-                <br />
-                <h2>Trail Blazers News</h2>
-                <ul>
-                    {mappedArr}
-                </ul>
-            </div>
-        );
-    }
+function TeamPage() {
+    const { team } = useParams();
+
+    return (
+        <div className='TeamPage'>
+            <h1>Welcome the Team Page for the { team }!!</h1>
+            <h3>Here is where all the magic happens!!</h3>
+            <h4>Boom Boom Clap!</h4>
+        </div>
+    )
 }
 
 export default TeamPage;
